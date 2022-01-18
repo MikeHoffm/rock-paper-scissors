38394f32af132f297013dc7788e7ac92207a09be
Mike Hoffm <mhoffm24@gmail.com>
2022-01-18 15:20:34 -0500

    README.md rps.html and scripts.js edits

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # rock-paper-scissors
-A rock, paper, scissors application using basic JavaScript.
+A rock paper scissors application using basic JavaScript.
 
 This program gave me a chance to put the following practices to work:
 Functions, Pseudocode, Arrays, Conditional Satements.
+
+Some challenges faced during this project:
+ - Computer move choices. This was achieved using arrays, and methods.
+ - Making it possible for the user to make a new play each round. I initially had the prompt for userPlay set outside of the playRound function (in the global scope). This caused an issue when I wanted to call playRound 5 times for a full game. The computer would be able to make a new move, but the user was only able to enter one move for all 5 rounds. Once I placed the userPlay prompt within the playRound function I got the results I wanted.
+ - Scoring. Eventually found a way to keep track of score by incrementing each time either the player, or computer won a round within playRound. Then within the playGame function I used conditional statements to determine who won based on the cumulative score of 5 rounds played.
+
+
+Things I've taken away from this project:
+ - Persistence is key. There were times when frustration would creep in, and if I had given up I wouldn't have been able to finish.
+ - Pay attention to where you're placing things within the local or global scope. This is simple but can lead to big problems.
--- scripts.js
+++ scripts.js
@@ -16,51 +16,55 @@
 } 
 
 /* Created a prompt for the user to make their play. As well as a system to change user input to become case sensitive. Ex: If user inputs "ROck" userPlay will change it to "Rock"*/
-
-let userPrompt = window.prompt('Choose your weapon: Rock, Paper, or Scissors', 'What will it be?')
-let userNew = userPrompt.toLowerCase();
-let userPlay = userNew.charAt(0).toUpperCase() + userNew.slice(1);
+// Having the players choice pre defined outside of the function will only allow for one answer to be reused
+//let userPrompt = window.prompt('Choose your weapon: Rock, Paper, or Scissors', 'What will it be?')
+//let userNew = userPrompt.toLowerCase();
+//let userPlay = userNew.charAt(0).toUpperCase() + userNew.slice(1);
 
 /*Created a function to play a single round of the game. */
 
 function playRound(playerSelection, computerSelection) {
+   let userPrompt = window.prompt('Choose your weapon: Rock, Paper, or Scissors', 'What will it be?')
+   let userNew = userPrompt.toLowerCase();
+   let userPlay = userNew.charAt(0).toUpperCase() + userNew.slice(1);
+
    computerSelection = computerPlay();
    playerSelection = userPlay;
-   let playerScore = 0;
-   let compScore = 0;
 
    if (playerSelection == computerSelection)
-      return "It\'s a tie!";
+      return ("It\'s a tie! " + "You both chose " + playerSelection + " Your score is " + playerScore + " AI score is " + compScore); 
    else if (playerSelection == "Paper" && computerSelection == "Rock")
-      return "You win, Paper beats Rock"; 
+      return ("You win!" + " " + "Paper beats Rock!!" + " " + "Player Choice: Paper, AI Choice: Rock " + " Your score is " + ++playerScore + " AI score is " + compScore);
    else if (playerSelection == "Rock" && computerSelection == "Scissors")
-      return "You win, Rock beats Scissors";
+      return ("You win!" + " " + "Rock beats Scissors!" + " " + "Player Choice: Rock, AI Choice: Scissors " + " Your score is " +  ++playerScore+ " AI score is " + compScore);
    else if (playerSelection == "Scissors" && computerSelection == "Paper")
-      return "You win, Scissors beats Paper";
+      return ("You win!" + " " + "Scissors beats Paper!" +  " " + "Player Choice: Scissors, AI Choice: Paper " + " Your score is " + ++playerScore+ " AI score is " + compScore);
    else if (computerSelection == "Paper" && playerSelection == "Rock")
-      return "You lose, Paper beats Rock";
+      return ("You lose!" + " " + "Paper beats Rock!" + " " +  "Player Choice: Rock, AI Choice: Paper " + " Your score is " + playerScore + " AI score is " + ++compScore);
    else if (computerSelection == "Rock" && playerSelection == "Scissors")
-      return "You lose, Rock beats Scissors ";
+      return ("You lose!" + " " +  "Rock beats Scissors!" + " " + "Player Choice: Scissors, AI Choice: Rock " + " Your score is " + playerScore + " AI score is " + ++compScore);
    else if (computerSelection == "Scissors" && playerSelection == "Paper")
-      return "You lose, Scissors beats Paper ";
+      return ("You lose!" + " " +  "Scissors beats Paper!" + " " + "Player Choice: Paper, AI Choice: Scissors " + " Your score is " + playerScore + " AI score is " + ++compScore);
 }
 //Write a function that plays 5 rounds AND KEEPS SCORE
-//ADD A WAY TO CHANGE YOUR PICK OF ROCK/PAPER/SCISSORS ON EACH TURN OUT OF 5
+//Set the player & comp scores outside of the function playRound this allowed the scores to be incremented each time a round was won
+//Created the function game() that calls playRound() 5 times. At the end of these 5 rounds, the game() function decides a winner based on score.
+let playerScore = 0;
+let compScore = 0;
 
-//MAYBE SOMETHING LIKE THIS?
-//function game() {
-  /* console.log(playRound() 
-  if round ends with player scoring a point. Display point
-   console.log(playRound second time)
-   if round ends with player scoring another point show cumulative points
-   if round ended with computer scoring point display the comp points as well as players
-   find a way to do this 5 times and declare a winner based on points
-}*/
+function playGame() {
+   console.log(playRound());
+   console.log(playRound());
+   console.log(playRound());
+   console.log(playRound());
+   console.log(playRound());
+   if (playerScore > compScore)
+      console.log ("You won the game")
+   else if (playerScore < compScore)
+      console.log ("You lost the game")
+   else if (playerScore == compScore)
+      console.log("It\'s a tied game")
 
-
-//Create a function to keep score of the game & declare a winner based on that score
-/*function keepScore(){
 }
 
-console.log(game());
-*/
+// Use playGame(); function within the console to activate the Rock Paper Scissors game
